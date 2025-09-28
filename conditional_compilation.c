a7209d52309353e5c86cf334700f783aaddae6d1
nikhil <nikhilkumaraf309@gmail.com>
2025-09-28 14:38:02 +0530

    final

---
+++ conditional_compilation.c
@@ -0,0 +1,18 @@
+// program to illusttrate conditional compilation
+
+#include <stdio.h>
+
+#define code
+
+int main()
+{
+    printf("a\n");
+    #ifdef code
+       printf("b\n");
+    #else
+       printf("c\n");
+    #endif
+       printf("d\n");
+        
+    return 0;
+}
